b23f7ce1067c394397cbf12feaed38194236a152
lsantamaria <luis.santamaria.sola@gmail.com>
2019-02-06 16:27:28 +0100

    Initial commit

---
+++ src/main/java/com/codetest/lsantamaria/creditcardprocessing/CreditCardProcessingApplication.java
@@ -0,0 +1,14 @@
+package com.codetest.lsantamaria.creditcardprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardProcessingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CreditCardProcessingApplication.class, args);
+  }
+
+}
+
---
+++ src/test/java/com/codetest/lsantamaria/creditcardprocessing/CreditCardProcessingApplicationTests.java
@@ -0,0 +1,17 @@
+package com.codetest.lsantamaria.creditcardprocessing;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CreditCardProcessingApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
+
